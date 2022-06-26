a5c261cc18a28e2fe071734dc9496cca0ad3b09b
MichaelDiers <md@sreid.de>
2022-06-26 12:24:30 +0200

    delete invitations

--- miracle-hides-server/src/controllers/invitation-code/invitation-code.service.ts
+++ miracle-hides-server/src/controllers/invitation-code/invitation-code.service.ts
@@ -1,9 +1,10 @@
-import { ConflictException, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { ConflictException, Injectable, NotFoundException } from '@nestjs/common';
 import { InvitationCodesDatabaseService } from '../../databases/invitation-codes-database/invitation-codes-database.service';
 import { v4 as uuidv4 } from 'uuid';
 import { CreateDto } from './create.dto';
 import { HashService } from 'src/services/hash/hash.service';
 import { InvitationCode } from 'src/dtos/invitation-code.interface';
+import { DeleteDto } from './delete.dto';
 
 @Injectable()
 export class InvitationCodeService {
@@ -26,5 +27,27 @@ export class InvitationCodeService {
     });
 
     return { code, email };
+  }
+
+  public async deleteAsync(deleteDto: DeleteDto) : Promise<void> {
+    if (!await this.invitationCodesDatabaseService.deleteByCodeAsync(deleteDto.code)) {
+      throw new NotFoundException();
+    }
+  }
+
+  public async readAsync() : Promise<InvitationCode[]> {
+    const iterator = this.invitationCodesDatabaseService.readAllAsync();
+    let current = iterator.next();
+    const invitations = [];
+    while (!(await current).done) {
+      const value = (await current).value;
+      if (value) {
+        invitations.push(...value)
+      }
+
+      current = iterator.next();
+    }
+
+    return invitations;
   }
 }
