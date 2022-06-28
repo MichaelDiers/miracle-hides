9f33c314092b0a328cc9f5bd388b2fe4632b5130
MichaelDiers <md@sreid.de>
2022-06-28 11:47:50 +0200

    prepare server refactoring

--- miracle-hides-server/src/app.module.ts
+++ miracle-hides-server/src/app.module.ts
@@ -5,19 +5,20 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from './services/config/config.module';
 import { ConfigService } from './services/config/config.service';
 import { UsersDatabaseModule } from './databases/users-database/users-database.module';
-import { AuthModule } from './controllers/auth/auth.module';
+import { AuthModule } from './controllers2/auth/auth.module';
 import { HashModule } from './services/hash/hash.module';
 import { FirebaseModule } from './services/firebase/firebase.module';
 import { JwtModule } from './services/jwt/jwt.module';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
-import { MessagesModule } from './controllers/messages/messages.module';
+import { MessagesModule } from './controllers2/messages/messages.module';
 import { MessagesDatabaseModule } from './databases/messages-database/messages-database.module';
-import { UserModule } from './controllers/user/user.module';
-import { InvitationCodeModule } from './controllers/invitation-code/invitation-code.module';
+import { UserModule } from './controllers2/user/user.module';
+import { InvitationCodeModule } from './controllers2/invitation-code/invitation-code.module';
 import { InvitationCodesDatabaseModule } from './databases/invitation-codes-database/invitation-codes-database.module';
 import { MailerModule } from './services/mailer/mailer.module';
-import { ChatModule } from './controllers/chat/chat.module';
+import { ChatModule } from './controllers2/chat/chat.module';
+import { ControllersModule } from './controllers/controllers.module';
 
 @Module({
   imports: [
@@ -44,6 +45,7 @@ import { ChatModule } from './controllers/chat/chat.module';
     InvitationCodesDatabaseModule,
     MailerModule,
     ChatModule,
+    ControllersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
