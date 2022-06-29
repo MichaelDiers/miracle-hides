f5f995d5bfb0eda2b52440ea807a7d9f153cd358
MichaelDiers <md@sreid.de>
2022-06-29 22:57:58 +0200

    link to chat invitation

--- miracle-hides-server/src-client/plain/js/pages/messenger-page.js
+++ miracle-hides-server/src-client/plain/js/pages/messenger-page.js
@@ -1,10 +1,12 @@
 class MessengerPage extends BasePage {
-  constructor({ translator, customEventName, errorEventName, requestAddChatPage }) {
+  constructor({ translator, customEventName, errorEventName, requestAddChatPage, requestAddChatInvitationPage }) {
     super({ translator, customEventName, errorEventName, id: 'messanger' });
 
     this.__createChatId = 'createChat';
+    this.__createChatInvitationId = 'createChatInvitation';
 
     this.__requestAddChatPage = requestAddChatPage;
+    this.__requestAddChatInvitationPage = requestAddChatInvitationPage;
   }
 
   async setup() {
@@ -21,12 +23,21 @@ class MessengerPage extends BasePage {
       ${HtmlComponents.linkButton({
         translationTag: BaseTranslator.keys.messengerCreateChat,
         id: this.__createChatId })}
+        ${HtmlComponents.linkButton({
+          translationTag: BaseTranslator.keys.messengerCreateChatInvitation,
+          id: this.__createChatInvitationId })}
     `;
 
     this.handleLink({
       root: div,
       sourceId: this.__createChatId,
       showPageEventName: this.__requestAddChatPage,
+    });
+
+    this.handleLink({
+      root: div,
+      sourceId: this.__createChatInvitationId,
+      showPageEventName: this.__requestAddChatInvitationPage,
     });
     
     return [...div.children];
