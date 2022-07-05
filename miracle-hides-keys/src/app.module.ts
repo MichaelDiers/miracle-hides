7a60e2e5f5b5accfeaef3b861af2b84da4d097a2
MichaelDiers <md@sreid.de>
2022-07-05 09:11:48 +0200

    add client and handle static files

--- miracle-hides-keys/src/app.module.ts
+++ miracle-hides-keys/src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ControllersModule } from './controllers/controllers.module';
+import { join } from 'path';
 import { KeyGeneratorModule } from './use-cases/key-generator/key-generator.module';
 import { RsaKeyGeneratorModule } from './use-cases/rsa-key-generator/rsa-key-generator.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
 import { UtilsModule } from './core/utils/utils.module';
 
 @Module({
@@ -9,6 +11,9 @@ import { UtilsModule } from './core/utils/utils.module';
     ControllersModule,
     KeyGeneratorModule,
     RsaKeyGeneratorModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, 'client'),
+    }),
     UtilsModule,
   ],
 })
