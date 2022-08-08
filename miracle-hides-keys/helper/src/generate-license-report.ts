f6a76af75fa6a2d52999f1d54176f188fc8be1c4
MichaelDiers <md@sreid.de>
2022-08-08 14:55:52 +0200

    collect license-report data

---
+++ miracle-hides-keys/helper/src/generate-license-report.ts
@@ -0,0 +1,54 @@
+import * as fs from 'fs';
+
+const directory = `${__dirname}/../license-report/`;
+
+const report = {
+  node: undefined,
+  fonts: [
+    {
+      font: 'Orbitron',
+      license: 'Open Font License 1.1',
+      author: 'Matt McInerney',
+      link: 'https://fonts.google.com/specimen/Orbitron'
+    },
+    {
+      font: 'Ubuntu Condensed',
+      license: 'Ubuntu Font License 1.0',
+      author: 'Dalton Maag',
+      link: 'https://fonts.google.com/specimen/Ubuntu+Condensed'
+    }
+  ]
+}
+
+fs.readdir(directory, (err, files) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  const nodeReport = [];
+  files.filter((file) => file.endsWith('.json')).forEach((file) => {
+    const path = `${directory}${file}`;
+    const entries = require(path);
+    entries.forEach((entry) => {
+      const { name: entryName } = entry;
+      if (!nodeReport.find(({ name }) => name === entryName)) {
+        nodeReport.push(entry);
+      }
+    });
+  });
+
+  nodeReport.sort((a, b) => {
+    if (a.name < b.name) {
+      return -1;
+    } else if (a.name > b.name) {
+      return 1;
+    }
+
+    return 0;
+  });
+  report.node = nodeReport;
+  console.log('const REPORT =', report, ';');
+  console.log();
+  console.log('export default REPORT;');    
+});
