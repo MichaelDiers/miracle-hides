28ba12fbcc00950e0bc9757656db3ba33dda2ee3
MichaelDiers <md@sreid.de>
2022-08-24 20:55:39 +0200

    disable request logger for production

--- miracle-hides-tabletop/miracle-hides-tabletop-server/src/middleware/request-logger.middleware.ts
+++ miracle-hides-tabletop/miracle-hides-tabletop-server/src/middleware/request-logger.middleware.ts
@@ -2,8 +2,15 @@ import { Injectable, NestMiddleware } from '@nestjs/common';
 
 @Injectable()
 export class RequestLoggerMiddleware implements NestMiddleware {
-  use(req: any, res: any, next: () => void) {    
-    console.log(`${req.method} ${req.originalUrl} ${JSON.stringify(req.body)} ${req?.headers?.authorization}`)
+  use(req: any, res: any, next: () => void) {
+    if (process.env.MH_REQUEST_LOGGER) {
+      const method = req.method;
+      const url = req.originalUrl;
+      const body = JSON.stringify(req.body);
+      const token = req?.headers?.authorization;
+      console.log(`${method} ${url} ${body} ${token}`);
+    }
+
     next();
   }
 }
